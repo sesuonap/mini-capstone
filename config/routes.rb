a27fc739a3fdb4f7de1e173fcffb963bb06c7640
sesuonap <tpanouses@gmail.com>
2018-05-15 21:31:35 -0500

    5/18

--- config/routes.rb
+++ config/routes.rb
@@ -7,3 +7,6 @@ Rails.application.routes.draw do
     delete '/products/:id' => 'products#destroy'
   end 
 end
+
+
+
---
+++ db/migrate/20180516021955_make_changes_to_description.rb
@@ -0,0 +1,8 @@
+class MakeChangesToDescription < ActiveRecord::Migration[5.1]
+  def change
+    change_column :products, :description, :text
+    change_column :products, :price, "numeric USING CAST(price AS numeric)"
+    change_column :products, :price, :decimal, precision: 9, scale: 2
+    add_column :products, :stock, :integer
+  end
+end
